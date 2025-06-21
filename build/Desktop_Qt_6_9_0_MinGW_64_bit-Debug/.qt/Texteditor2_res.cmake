86199736d8d6e4af36acdf237e53cd4209bf7474
Vidhi425 <hcpsvidhi25@gmail.com>
2025-06-21 12:01:49 +0530

    initial commit

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/Texteditor2_res.cmake
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/Texteditor2_res.cmake
@@ -4,6 +4,8 @@ set(working_dir "C:/Project/opencv/Texteditor2")
 set(src_and_dest_list
     "C:/Project/opencv/Texteditor2/images-asset.qrc"
     "C:/Project/opencv/Texteditor2/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/Texteditor2/images-asset.qrc"
+    "C:/Project/opencv/Texteditor2/.gitignore"
+    "C:/Project/opencv/Texteditor2/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/Texteditor2/.gitignore"
 
 )
 set(timestamp_file "C:/Project/opencv/Texteditor2/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/Texteditor2_res.txt")
